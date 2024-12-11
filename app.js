e09906445c786064a1eb60b10a17abb6ce9352c9
jacob <jacobthompson708@gmail.com>
2024-12-10 23:50:37 -0600

    updated

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res){
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
     res.send('Webpage created with express and node.js for testing CI/CD');
 });
 
-app.listen(3000, function(){
-    console.log('Example app listening on port 3000!');
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
+
+process.on('SIGINT', () => {
+    console.log('SIGINT received, shutting down');
+    server.close(() => {
+        console.log('Server closed.');
+        process.exit(0);
+    });
+})
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/myapp
+pm2 start app.js
